b13813d0b864fcbb31ea082c0786a2b794a6a9f1
enqer <pawel.kam4@gmail.com>
2023-11-15 16:31:34 +0100

    update not found

--- src/components/notFound.jsx
+++ src/components/notFound.jsx
@@ -2,15 +2,29 @@ import {Link} from "react-router-dom";
 
 const NotFound = () => {
     return(
-        <>
+        <div style={styles.all}>
             <h1>NIE ZNALEZIONO STRONY O PODANYM ADRESIE.</h1>
-            <p>
+            <p style={styles.p}>
                 Przepraszamy. Strona, której szukasz nie została odnaleziona.
+                <br />
+                Sprawdź poprawność adresu lub zajrzyj na stronę <Link to="/" style={styles.link}>startową naszego serwisu.</Link>
             </p>
-            <p>
-                Sprawdź poprawność adresu lub zajrzyj na stronę <Link to="/">startową naszego serwisu.</Link>
-            </p>
-        </>
+        </div>
     )
+}
+const styles = {
+    all: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        alignContent: 'center'
+    },
+    p : {
+        textAlign: 'center'
+    },
+    link : {
+        color: '#4682b4',
+        textDecoration: 'none'
+    }
 }
 export default NotFound
