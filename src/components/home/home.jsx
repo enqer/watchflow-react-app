222d86cf71ed1b7495f5c09a87fedad2f3dd1224
enqer <pawel.kam4@gmail.com>
2023-12-29 00:13:00 +0100

    feat: display popular movies

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,11 +3,29 @@ import styles from './home.module.css'
 import img from "../../img/background.jpg";
 import img2 from "../../img/news.jpg";
 import NewsCard from "../news/newsCard";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import MovieCard from "../movies/movieCard";
 
 const Home = () => {
         const arr = [...Array(10).keys()]
         const arr2 = [...Array(3).keys()]
-// TODO nowy komponent za te kafelki?? po prawej u góry gwiazdki a na środku tytuł??
+        const [data, setData] = useState([])
+
+
+        const getPopularMovies = () => {
+                axios
+                    .get(`http://localhost:8080/api/movies/popular?last=${5}`)
+                    .then((response) => {
+                            setData(response.data)
+                    })
+                    .catch((error) => console.log(error))
+        }
+
+        useEffect(() => {
+                getPopularMovies()
+        }, []);
+
         return (
             <div className={styles.container}>
                     <p className={styles.popularText}>Odkryj nowości</p>
@@ -23,11 +41,11 @@ const Home = () => {
                 <div >
                         <p className={styles.popularText}>Najpopularniejsze</p>
                         <div className={styles.wrapperImages}>
-                                {arr.map(a => (
-                                    <div className={styles.movieCell}>
-                                            <img className={styles.moviesImg}  src={img}/>
-                                    </div>
-                                ))}
+                                {
+                                        data.map((movie) => (
+                                            <MovieCard id={movie.id} img={movie.image} title={movie.title} />
+                                        ))
+                                }
                         </div>
                 </div>
             </div>
